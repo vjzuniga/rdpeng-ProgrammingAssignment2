8fd11b092a610ee9d0426c0904f99e4e3da81179
vjzuniga <vjzuniga@reduno.com.mx>
2016-02-22 08:30:25 -0600

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix() ) {
+        MInversa  <- NULL
+        set <- function(y) {
+                x <<- y
+                MInversa  <<- NULL
+        }
+        get <- function() x
+        setMInversa <- function(solve) MInversa <<- solve
+        getMInversa <- function() MInversa
+        list(set = set, get = get,
+             setMInversa = setMInversa,
+             getMInversa = getMInversa)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        MInversa <- x$getMInversa()
+        if(!is.null(MInversa)) {
+                message("getting cached data")
+                return(MInversa)
+        }
+        data <- x$get()
+       MInversa<- solve(data, ...)
+        x$setMInversa(MInversa)
+        MInversa
 }
